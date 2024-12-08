c5c852242d426fe917897b6ee375f1dadb3f8709
chinmay padhi <padhichinmay108@gmail.com>
2024-12-08 12:23:36 +0530

    first commit

---
+++ Fibonacy series/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Fibonacy_series
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           int no= 10;
+
+            int firstNo = 0, SecondNo = 1, nextNo;
+
+            for(int i=2; i<no;i++)
+            {
+                nextNo = firstNo + SecondNo;
+                Console.WriteLine(nextNo);
+                firstNo = SecondNo;
+                SecondNo = nextNo;
+
+            }
+
+            Console.ReadKey();
+
+
+        }
+    }
+}
